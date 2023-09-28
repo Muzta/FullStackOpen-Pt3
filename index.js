f6855bc9e6886479bef9c6edd2dd309d905ce728
muzta <javyngs@gmail.com>
2023-09-28 19:51:04 +0200

    Finished 'phonebook backend, step 8'

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("person-data", (req, res) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  }
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person-data"
+  )
+);
 
 let phonebook = [
   {
