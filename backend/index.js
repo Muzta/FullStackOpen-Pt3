e3876ed6220072692c4de6512e3ef40e0e86e376
muzta <javyngs@gmail.com>
2023-10-02 20:36:35 +0200

    Finished 'phonebook database, step 1'

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
@@ -18,37 +20,53 @@ app.use(
   )
 );
 
-let phonebook = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+const url = `mongodb+srv://fullstack:${process.env.MONGO_PASSWORD}@cluster0.stqut0q.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+// let phonebook = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
 app.get("/info", (request, response) => {
   const requestDate = new Date();
   const res = `<p>Phonebook has info for ${phonebook.length} people</p><p>${requestDate}</p>`;
   response.send(res).end();
-  // "<p>Phonebook has info for  people</p><p></p>"
 });
 
-app.get("/api/persons", (request, response) => response.json(phonebook));
+app.get("/api/persons", (request, response) =>
+  Person.find({}).then((person) => {
+    response.json(person);
+    mongoose.connection.close();
+  })
+);
 
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
