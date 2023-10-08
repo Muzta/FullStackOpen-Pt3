d67b723ffc2029aa1290a37ca50cdb091f73caf8
muzta <javyngs@gmail.com>
2023-10-08 17:48:43 +0200

    Finished ex 3.19

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,5 +1,7 @@
 module.exports = (error, request, response, next) => {
   if (error.name === "CastError")
     return response.status(400).send({ error: "Malformatted id" });
+  else if (error.name === "ValidationError")
+    return response.status(400).json({ error: error.message });
   next(error);
 };
